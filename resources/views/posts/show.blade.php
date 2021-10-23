62a0fac4e8f82c8073eb262b7a70655e94232ebd
muhammad <muhammadfallahi6@gmail.com>
2021-10-23 10:40:02 +0330

    add post show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,16 +4,13 @@
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-lg-8">
-              @foreach ($posts as $post)
               <div class="card mt-3">
                 <h5 class="card-header">{{ $post->user->name }}</h5>
                 <div class="card-body">
                   <h5 class="card-title">{{ $post->title }}</h5>
                   <p class="card-text">{{ $post->content }}</p>
-                  <a href="{{ route('post.show', [$post]) }}" class="btn btn-primary">view</a>
                 </div>
               </div>
-              @endforeach
             </div>
 
         </div>
