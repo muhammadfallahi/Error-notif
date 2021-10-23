1a82da702479152441b956851f4210cecb8a72e8
muhammad <muhammadfallahi6@gmail.com>
2021-10-23 11:50:47 +0330

    add pagination to posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,24 +7,27 @@
                 @if (session('message'))
                     <div class="alert alert-success" role="alert">{{ session('message') }}</div>
                 @endif
-              @foreach ($posts as $post)
-              <div class="card mt-3">
-                <h5 class="card-header">{{ $post->user->name }}</h5>
-                <div class="card-body">
-                  <h5 class="card-title">{{ $post->title }}</h5>
-                  <p class="card-text">{{ Str::limit($post->content, 50, $end='...') }}</p>
-                  <div class="d-flex">
-                  <a href="{{ route('post.show', [$post]) }}" class="btn btn-primary ml-2">view</a>
-                  <a href="{{ route('post.edit', [$post]) }}" class="btn btn-warning ml-2">edit</a>
-                  <form action="{{ route('post.destroy', [$post]) }}" method="post" class="ml-2">
-                    @csrf
-                    @method('DELETE')
-                      <button class="btn btn-danger">delete</button>
-                  </form>
+                @foreach ($posts as $post)
+                    <div class="card mt-3">
+                        <h5 class="card-header">{{ $post->user->name }}</h5>
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $post->title }}</h5>
+                            <p class="card-text">{{ Str::limit($post->content, 50, $end = '...') }}</p>
+                            <div class="d-flex">
+                                <a href="{{ route('post.show', [$post]) }}" class="btn btn-primary ml-2">view</a>
+                                <a href="{{ route('post.edit', [$post]) }}" class="btn btn-warning ml-2">edit</a>
+                                <form action="{{ route('post.destroy', [$post]) }}" method="post" class="ml-2">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger">delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+                <div class="mt-3">
+                    {{ $posts->links() }}
                 </div>
-                </div>
-              </div>
-              @endforeach
             </div>
 
         </div>
